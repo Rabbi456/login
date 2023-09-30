8799b622589bda68a6a631c9efa50b7ce164031d
Rabbi456 <Karismaraboni@gmail.com>
2023-09-30 21:39:17 +0000

    uncomment window.onresize

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
 // window.onload(alert("hello world"))
 // console.log("hello world")
 
-// window.onresize(alert("I have been resized"))
+ window.onresize(alert("I have been resized"))
 
 function myFunction(){
     alert("form submitted")
